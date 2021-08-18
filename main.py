f00c2fa4da697d229ad0ca33bf1e2ee76de95792
James Phillips <jmphillips476@gmail.com>
2021-08-18 03:21:27 -0500

    Saving along the way. Starting the blog setup.

--- main.py
+++ main.py
@@ -2,11 +2,29 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+"""
+When setting up paths, you should put the exact matches above any dynamic blocks.
 
-@app.get('/')
-def index():
-    return {'data':{'name':'Matthew'}}
+"""
 
-@app.get('/about')
-def about():
-    return {'data':'about page'}
+@app.get('/blog') # get blogs-- get operation
+def index(limit, published:bool):
+    if published:
+        return {'data':f'{limit} publsihed blogs from the db'}
+    else:
+        return {'data':f'{limit} blogs from the db'}
+
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data':'all unpublished blogs'}
+
+
+@app.get('/blog/{id}') #single blog show path -- get operation
+def show(id:int):
+    #fetch blog with id = id
+    return {'data': id}
+
+@app.get('/blog/{id}/comments')
+def comments(id):
+    #fetch comments of blog with id = id
+    return {'data': {'1','2'}}
