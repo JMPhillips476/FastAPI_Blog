c9dd9b56acc6bff0712e8acbf3be1ba4872430de
James Phillips <jmphillips476@gmail.com>
2021-08-18 07:22:30 -0500

    Added Update and Delete functionality as well as error handling.

--- blog/main.py
+++ blog/main.py
@@ -25,7 +25,28 @@ def create(request : schemas.Blog, db : Session = Depends(get_db)):
     db.refresh(new_blog)
     return new_blog
 
+@app.delete('/blog/{id}',status_code=status.HTTP_204_NO_CONTENT)
+def destroy(id, db : Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
 
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id {id} not found")
+    
+    blog.delete()
+    db.commit()
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
+@app.put('/blog/{id}',status_code=status.HTTP_202_ACCEPTED)
+def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id {id} not found")
+    blog.update(request.dict())
+
+    db.commit()
+    return 'updated succesfully'
 
 @app.get('/blog')
 def all_blogs(db : Session = Depends(get_db)):
