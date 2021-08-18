7fc7a33e335b3911ae5f0712257fb3fd6972c473
James Phillips <jmphillips476@gmail.com>
2021-08-18 08:03:28 -0500

    Added Get user and User Response Schema

--- blog/main.py
+++ blog/main.py
@@ -57,7 +57,7 @@ def all_blogs(db : Session = Depends(get_db)):
 
 
 
-@app.get('/blog/{id}',status_code=200,response_model=schemas.ShowBlog)
+@app.get('/blog/{id}',status_code=status.HTTP_200_OK,response_model=schemas.ShowBlog)
 def show(id, response : Response, db : Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
 
@@ -71,10 +71,19 @@ def show(id, response : Response, db : Session = Depends(get_db)):
 
 
 
-@app.post('/user', status_code=status.HTTP_201_CREATED)
+@app.post('/user', status_code=status.HTTP_201_CREATED, response_model=schemas.ShowUser)
 def create_user(request: schemas.User, db : Session = Depends(get_db)):
     new_user = models.User(name = request.name, email = request.email, password = Hash.bcrypt(request.password))
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
     return new_user
+
+
+@app.get('/user/{id}', status_code=status.HTTP_200_OK, response_model=schemas.ShowUser)
+def get_user(id:int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+
+    if not user:
+         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail = f"User with the id {id} is not available")
+    return user
