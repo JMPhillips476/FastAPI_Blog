210fbbac0027cb0fb6d36b641653e33bc418e7a0
James Phillips <jmphillips476@gmail.com>
2021-08-18 07:43:03 -0500

    Add new user (no encryption on password yet)

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,15 @@ from .database import Base
 
 class Blog(Base):
     __tablename__ = "Blogs"
-    
+
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String)
     body = Column(String)
+
+class User(Base):
+    __tablename__ = "Users"
+
+    id = Column(Integer, primary_key=True, index= True)
+    name = Column(String)
+    email = Column(String)
+    password = Column(String)
