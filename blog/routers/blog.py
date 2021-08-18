2135b87c7dae6521c275cb44c9d863a865d119c4
James Phillips <jmphillips476@gmail.com>
2021-08-18 10:02:46 -0500

    Cleaning up routers by moving functionality to driver files.

--- blog/routers/blog.py
+++ blog/routers/blog.py
@@ -1,9 +1,9 @@
 from typing import List
-from fastapi import FastAPI, Depends, status, Response, HTTPException, APIRouter
-from pydantic.networks import HttpUrl
-from .. import schemas, models
+from fastapi import Depends, status, APIRouter
+from .. import schemas
 from ..database import SessionLocal, engine, get_db
 from sqlalchemy.orm import Session
+from ..repository import blogDriver as blog
 
 
 router = APIRouter(
@@ -13,46 +13,22 @@ router = APIRouter(
 
 @router.get('/', response_model=List[schemas.ShowBlog], tags=['get'])
 def all_blogs(db : Session = Depends(get_db)):
-    blogs = db.query(models.Blog).all()
-    return blogs
+    return blog.get_all(db)
 
 @router.post('/', status_code=status.HTTP_201_CREATED, tags=['post'])
 def create(request : schemas.Blog, db : Session = Depends(get_db)):
-    new_blog = models.Blog(title=request.title, body=request.body, user_id = 1) #!temp hardcoding
-    db.add(new_blog)
-    db.commit()
-    db.refresh(new_blog)
-    return new_blog
+    return blog.create(request, db)
 
 @router.delete('/{id}',status_code=status.HTTP_204_NO_CONTENT, tags=['delete'])
-def destroy(id, db : Session = Depends(get_db)):
-    blog = db.query(models.Blog).filter(models.Blog.id == id)
-
-    if not blog.first():
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id {id} not found")
-    
-    blog.delete()
-    db.commit()
-    return Response(status_code=status.HTTP_204_NO_CONTENT)
+def destroy(id:int, db : Session = Depends(get_db)):
+    return blog.destroy(id, db)
 
 
 @router.put('/{id}',status_code=status.HTTP_202_ACCEPTED, tags=['put'])
-def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
-    blog = db.query(models.Blog).filter(models.Blog.id == id)
-
-    if not blog.first():
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id {id} not found")
-    blog.update(request.dict())
-
-    db.commit()
-    return 'updated succesfully'
+def update(id:int, request: schemas.Blog, db: Session = Depends(get_db)):
+    return blog.update(id, request, db)
 
 
 @router.get('/{id}',status_code=status.HTTP_200_OK,response_model=schemas.ShowBlog, tags=['get'])
-def show(id, response : Response, db : Session = Depends(get_db)):
-    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
-
-    if not blog:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail = f"Blog with the id {id} is not available")
-
-    return blog
+def show(id:int, db : Session = Depends(get_db)):
+    return blog.getPost(id, db)
--- blog/routers/user.py
+++ blog/routers/user.py
@@ -1,10 +1,8 @@
-from typing import List
-from fastapi import FastAPI, Depends, status, Response, HTTPException, APIRouter
-from pydantic.networks import HttpUrl
-from .. import schemas, models
-from ..database import SessionLocal, engine, get_db
+from fastapi import Depends, status, APIRouter
+from .. import schemas
+from ..database import get_db
 from sqlalchemy.orm import Session
-from ..hashing import Hash
+from ..repository import userDriver as user
 
 router = APIRouter(
     tags = ['user'],
@@ -12,18 +10,10 @@ router = APIRouter(
 )
 
 @router.post('/', status_code=status.HTTP_201_CREATED, response_model=schemas.ShowUser, tags=['post'])
-def create_user(request: schemas.User, db : Session = Depends(get_db)):
-    new_user = models.User(name = request.name, email = request.email, password = Hash.bcrypt(request.password))
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
-    return new_user
+def create_user(request: schemas.UserBase, db : Session = Depends(get_db)):
+    return user.create(request, db)
 
 
 @router.get('/{id}', status_code=status.HTTP_200_OK, response_model=schemas.ShowUser, tags=['get'])
-def get_user(id:int, db: Session = Depends(get_db)):
-    user = db.query(models.User).filter(models.User.id == id).first()
-
-    if not user:
-         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail = f"User with the id {id} is not available")
-    return user
+def getSingle(id:int, db: Session = Depends(get_db)):
+    return user.getSingle(id, db)
