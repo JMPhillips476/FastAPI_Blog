b37404f75b0d19360e209eab37e154f26947762e
James Phillips <jmphillips476@gmail.com>
2021-08-20 05:28:32 -0500

    Added JWT token use.

--- blog/routers/authentication.py
+++ blog/routers/authentication.py
@@ -1,3 +1,4 @@
+from blog.token import create_access_token
 from fastapi import APIRouter, Depends, status
 from fastapi.exceptions import HTTPException
 from .. import schemas, models
@@ -15,4 +16,9 @@ def login(request:schemas.Login, db: Session = Depends(get_db)):
     user = db.query(models.User).filter(models.User.email == request.username).first()
     if not user or not Hash.verify(user.password,request.password):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Invalid Credentials")
-    return user
+    
+    
+    access_token = create_access_token( data = {"sub" : user.email})
+
+
+    return {"access_token": access_token, "token_type": "bearer"}
